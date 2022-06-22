2be364300ec9cb554f0307c2fd7895cabc954150
nitish kumar <ns7410995@gmail.com>
2022-06-22 22:25:18 +0530

    update script.js

--- block-BJaaek/code/script.js
+++ block-BJaaek/code/script.js
@@ -1,19 +1,32 @@
 let box1 = document.querySelector('.first');
 let box2 = document.querySelector('.second');
 
-function colorchanging(){
-    var b = Math.floor(Math.random()*200);
-    var a = Math.floor(Math.random()*200);
-    var c = Math.floor(Math.random()*200);
-    var bgcolor = "rgb(" + a + ", " + b + ", " + c + ")";
-    return bgcolor;
-}
+function generaterandomcolor(){
+    let hexcharacter = ["0", "1", "2", "3", "4", "5","6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
+    let color = "#";
+    for (let i =0 ; i < 6; i++){
+        let randomNumber = Math.floor(Math.random()*16 );
+     color = color + hexcharacter[randomNumber];
+     }
+    return color;
+    }
 
-box1.addEventListener('click' , function(){
-    box1.style.background = colorchanging();
-});
+    function handleClick(){
+        // 1. generate random color
+        let randomcolor =  generaterandomcolor ();
+        // 2. change the background color of the box  
+        box1.style.background = randomcolor;
+
+    }
+    function handlemousemove(){
+        // 1. generate random color
+        let randomcolor =  generaterandomcolor ();
+        // 2. change the background color of the box  
+        box2.style.background = randomcolor;
+
+    }
+
+box1.addEventListener('click' , handleClick);
     
-box2.addEventListener('mousemove' , function(){
-    box2.style.background = colorchanging();  
-});
+box2.addEventListener('mousemove' , handlemousemove);
 
